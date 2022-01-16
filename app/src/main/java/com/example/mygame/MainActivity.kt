9264b846f456e8ae97ea561596f73e3540274efc
SuyogMahajan <mahajansuyog9322@gmail.com>
2022-01-16 10:56:26 +0530

    Result Activity Added

--- app/src/main/java/com/example/mygame/MainActivity.kt
+++ app/src/main/java/com/example/mygame/MainActivity.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import kotlinx.android.synthetic.main.activity_main.*
+import android.content.Intent as Intent
+
+val key = "res"
 
 class MainActivity : AppCompatActivity(), View.OnClickListener{
 
@@ -12,11 +15,13 @@ class MainActivity : AppCompatActivity(), View.OnClickListener{
     var turns: Int = 0
     lateinit var board : Array<Array<Button>>
     var boardVisited = Array(3){IntArray(3)}
+   lateinit var n:Intent
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        n = Intent(this,results::class.java)
         board = arrayOf(
             arrayOf(btn1,btn2,btn3)
             , arrayOf(btn4,btn5,btn6)
@@ -99,7 +104,9 @@ class MainActivity : AppCompatActivity(), View.OnClickListener{
         }
 
         if(turns == 9){
-            tv1.text ="# Game Draw #"
+            n.putExtra(key,"Match Draw")
+            startActivity(n)
+            return
         }
 
         checkWinner()
@@ -109,16 +116,19 @@ class MainActivity : AppCompatActivity(), View.OnClickListener{
 
         // check for all rows
 
+
         for(i in 0..2) {
             if (boardVisited[i][0] == boardVisited[i][1] && boardVisited[i][0] == boardVisited[i][2]) {
                 if (boardVisited[i][0] == 1) {
-                    tv1.text = "Player X Wins"
+                    n.putExtra(key,"X Wins")
+                    startActivity(n)
                     makeAlldisAble()
                     return
                 } else if (boardVisited[i][0] == 0) {
-                    tv1.text = "Player O Wins"
+                    n.putExtra(key,"O Wins")
+                    startActivity(n)
                     makeAlldisAble()
-                    break
+                    return
                 }
             }
         }
@@ -127,11 +137,13 @@ class MainActivity : AppCompatActivity(), View.OnClickListener{
         for(i in 0..2){
             if(boardVisited[0][i] == boardVisited[1][i] && boardVisited[0][i] == boardVisited[2][i]){
                 if(boardVisited[0][i] == 1){
-                    tv1.text ="Player X Wins"
+                    n.putExtra(key,"X Wins")
+                    startActivity(n)
                     makeAlldisAble()
                     return
                 }else if(boardVisited[0][i] == 0){
-                    tv1.text ="Player O Wins"
+                    n.putExtra(key,"O Wins")
+                    startActivity(n)
                     makeAlldisAble()
                     return
                 }
@@ -140,11 +152,13 @@ class MainActivity : AppCompatActivity(), View.OnClickListener{
         // check for 2 diagonals
         if(boardVisited[0][0] == boardVisited[1][1] && boardVisited[0][0] == boardVisited[2][2]){
             if(boardVisited[0][0] == 1){
-                tv1.text ="Player X Wins"
+                n.putExtra(key,"X Wins")
+                startActivity(n)
                 makeAlldisAble()
                 return
             }else if(boardVisited[0][0] == 0){
-                tv1.text ="Player O Wins"
+                n.putExtra(key,"O Wins")
+                startActivity(n)
                 makeAlldisAble()
                 return
             }
@@ -152,11 +166,13 @@ class MainActivity : AppCompatActivity(), View.OnClickListener{
 
         if(boardVisited[0][2] == boardVisited[1][1] && boardVisited[0][2] == boardVisited[2][0]){
             if(boardVisited[0][2] == 1){
-                tv1.text ="Player X Wins"
+                n.putExtra(key,"X Wins")
+                startActivity(n)
                 makeAlldisAble()
                 return
             }else if(boardVisited[0][2] == 0){
-                tv1.text ="Player O Wins"
+                n.putExtra(key,"O Wins")
+                startActivity(n)
                 makeAlldisAble()
                 return
             }
@@ -178,6 +194,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener{
     private fun updateText(i: Int, j: Int, player: Boolean) {
         val t = if(player) "X" else "O"
         val v = if(player) 1 else 0
+
         board[i][j].apply {
             isEnabled = false;
             text = t
